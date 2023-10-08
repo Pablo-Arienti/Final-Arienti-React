80946940066235fa6d8169878fe36095a1fab064
Pablo-Arienti <125293831+Pablo-Arienti@users.noreply.github.com>
2023-10-08 14:19:51 -0300

    solucionado firebase

--- src/services/firebase/firebaseConfig.js
+++ src/services/firebase/firebaseConfig.js
@@ -1,19 +1,15 @@
-// Import the functions you need from the SDKs you need
+
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyARNI5rj2qyIe3wSfmELQWf3ElC2Vs8F_4",
-  authDomain: "hard-war-96f40.firebaseapp.com",
-  projectId: "hard-war-96f40",
-  storageBucket: "hard-war-96f40.appspot.com",
-  messagingSenderId: "517175228619",
-  appId: "1:517175228619:web:1e3d8b66f937497c60434e"
+  apiKey: "AIzaSyB5eNfsARFeaJx0hKvUVO2wemPHK95rZlY",
+  authDomain: "h4rd-ware.firebaseapp.com",
+  projectId: "h4rd-ware",
+  storageBucket: "h4rd-ware.appspot.com",
+  messagingSenderId: "364032488811",
+  appId: "1:364032488811:web:4eedc375382892d8ec082e"
 };
 
-// Initialize Firebase
+
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app)
